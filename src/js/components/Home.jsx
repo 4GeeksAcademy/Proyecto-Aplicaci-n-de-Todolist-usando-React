94b62bc62e1dde335284b9e5d9e93bd7c119acb1
Alejandro <alejandrorcc24@gmail.com>
2025-03-18 15:21:35 +0000

    corregido

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,111 +1,99 @@
 import React, { useEffect, useState } from "react";
 
 export default function Home = () {
-	const [todos, setTodos] = useState([]);
-	const [nuevoTodo, setNuevoTodo] = useState("");
+	const [task, setTask] = useState("");
+	const [tasks, setTasks] = useState([]);
 
-	const fetchTodos = () => {
+	useEffect(() => {
+		fatchTasks();
+	}, []);
+
+
+	const fatchTasks = () => {
 		fetch("https://playground.4geeks.com/todo/users", {
 			method: "GET",
-			headers: {
-				"Content-Type": "application/json",
-			},
+			headers: { "Content-Type": "application/json", },
 		})
-			.then((response) => {
-				if (!response.ok) {
-					throw new Error("obtener datos");
+			.then((response) => response.json())
+			.then((data) => {
+				console.log("Datos agregados:", data);
+				if (Array.isArray(data.todos)) {
+					setTasks(data.todos);
+				} else {
+					console.error("La APIno delvolvio una lista valida.");
 				}
-				return response.json();
 			})
-			.then((response.Json) => {
-		console.log(
-			"completa la api:",
-			JSON.stringify(Response.Json, null, 2)
-		);
+			.catch((error) => console.error("Error al obtener tareas:", error));
+	};
 
-		if (responseJson.all) {
-			setTodos(ResponseJson.all);
-		} else {
-			console.error("DEVOLVER LA PROPIEDAD");
+	const handleKeyDown = (indice) => {
+		if (e.key === "Enter" && task.trim() !== "") {
+			const newTask = { label: task, done: false };
+
+			fetch(`https://playground.4geeks.com/todo/todos/${indice}`, {
+				method: "POST",
+				headers: { "Content-type": "application/json" },
+				body: JSON.stringify(newTask),
+			})
+				.then((response) => response.json())
+				.then((creastedTask) => {
+					console.log("Tarea creada:", creastedTask);
+					setTasks([...tasks, creastedTask]);
+					setTask("");
+				})
+				.catch((error) => console.error("Error al agregar tarea:", error));
 		}
-	})
-		.catch ((error) => console.error("Error", error));
-};
+	};
 
-useEffect(() => {
-	fetchTodos();
-}, [])
+	const handleDelete = (todoId) => {
+		const numericId = parseInt(todoId, 10);
+		if (inNaN(numericId)) {
+			console.error("Error: ID de tarea no es un numero:", todoId);
+			return;
+		}
 
-useEffect(() => {
-	console.log("acualizar all", todos);
-}, [todos]);
+		console.log("Eliminar tarea con ID:", numericId);
 
-
-const handleClick = (e) => {
-	if (e.key === "click" && todos.trim() !== "") {
-		setTodos([...todos, nuevoTodo])
-		setNuevoTodo("");
-	}
-};
-
-
-const deleteTodo = (indice) => {
-	console.log("eliminar", indice);
-
-	fetch(`https://playground.4geeks.com/todo/todos/${indice}`, {
-		method: "DELETE",
-		headers: {
-			"Content-Type": "application/json",
-		},
-	})
-
-		.then((response) => {
-			if (!response.ok) {
-				throw new Error("eliminar la tarea");
-			}
+		fetch((reponse) => {
+			if (!reponse.ok) throw new Error("Error al eliminar la tarea");
 			return response.text();
 		})
-		.then(() => {
-			console.log("delete success");
-			setTodos(listaNueva.data.todos);
-			setTodos(todos.filter((todo) => todo.id !== indice));
-		})
-		.catch((error) => console.error("eliminat las tareas", error));
-};
+			.then((message) => {
+				console.log("Respuesta de la API:", message);
+				setTasks(tasks.filter((task.id !== numericId));
+			})
+			.catch((error) => console.error("Error al eliminar tarea:", error));
+	};
 
-const handleChange = (event) => {
-	setNuevoTodo(event.target.value);
-}
+	return (
+		<div className="text-center">
+			<h1 className="text-center mt-5">Todos!</h1>
 
-return (
-	<div className="text-center">
-		<h1 className="text-center mt-5">Todos!</h1>
+			<div className="container">
+				<div className="d-flex gap-2">
 
-		<div className="container">
-			<div className="d-flex gap-2">
+					<div></div>
+					<input type="text" className="form-control" value={setTasks} onChange={(e) => setTasks(e.target.value)} onAbort={handleKeyDown} />
+					<button onClick={handleKeyDown} className="btn btn-primary">
+						Agregar
+					</button>
+				</div>
 
-				<div></div>
-				<input type="text" className="form-control" value={nuevoTodo} onChange={(e) => setNuevoTodo(e.target.value)} onAbort={handleClick} />
-				<button onClick={handleClick} className="btn btn-primary">
-					Agregar
-				</button>
+
+
+				{/* <p>Nueva tarea: {nuevoTask} </p> */}
+
+				<ul className="list-group">
+					{task.map((task, indice) => {
+						return (
+							<li className={`list-group-item d-flex justify-content-between align-items-center ${indice % 2 === 0 ? "bg-light" : ""}`}>
+								{task.label} <button className="btn btn-danger" onClick={() => handleDelete(indice)}>X</button>
+							</li>
+						)
+					})}
+
+				</ul>
 			</div>
-
-
-
-			{/* <p>Nueva tarea: {nuevoTodo} </p> */}
-
-			<ul className="list-group">
-				{todos.map((todo, indice) => {
-					return (
-						<li className={`list-group-item d-flex justify-content-between align-items-center ${indice % 2 === 0 ? "bg-light" : ""}`}>
-							{todo.label} <button className="btn btn-danger" onClick={() => deleteTodo(indice)}>X</button>
-						</li>
-					)
-				})}
-
-			</ul>
 		</div>
-	</div>
-);
+	);
 };
